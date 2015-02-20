626714e9a5b6280b5cb1888a2c7de224c8a9c89f
Elias Baixas <elias.baixas@gmail.com>
2015-02-20 08:48:11 +0100

    Fixies

--- lib/contacts/version.rb
+++ lib/contacts/version.rb
@@ -2,8 +2,8 @@ module Contacts
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 2
-    TINY  = 5
-    PATCH = 5
+    TINY  = 8
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, TINY, PATCH].join('.')
   end
